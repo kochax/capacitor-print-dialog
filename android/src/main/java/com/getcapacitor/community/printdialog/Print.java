f6b86b9edfa87008aa580dbf1a960cc3d31efe32
Jovan Kocic <kochax94@gmail.com>
2021-10-28 14:19:47 +0200

    Init

---
+++ android/src/main/java/com/getcapacitor/community/printdialog/Print.java
@@ -0,0 +1,11 @@
+package com.getcapacitor.community.printdialog;
+
+import android.util.Log;
+
+public class Print {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
